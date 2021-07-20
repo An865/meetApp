d5f99b0940822ace18a47ba72377446a794578b7
An865 <77985358+An865@users.noreply.github.com>
2021-07-20 12:23:18 -0700

    alert.js added and citysearch.js includes alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
@@ -7,6 +7,7 @@ class CitySearch extends Component {
     query: '',
     suggestions: [],
     showSuggestions: undefined,
+    infoText: ''
   }
   
 
@@ -19,6 +20,18 @@ class CitySearch extends Component {
         query: value,
         suggestions,
       });
+      if(suggestions.length === 0){
+        this.setState({
+          query: value,
+          infoText: 'We can not find the city you are looking for. Please try another city',
+        });
+      } else {
+        return this.setState({
+          query: value,
+          suggestions,
+          infoText: '',
+        });
+      }
     };
 
     handleItemClicked = (suggestion) => {
@@ -33,7 +46,7 @@ class CitySearch extends Component {
    render() {
      return (
       <div className="CitySearch">
-        
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
